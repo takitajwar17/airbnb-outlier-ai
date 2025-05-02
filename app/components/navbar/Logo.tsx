c266796a71e5bb99e4d46d48550b1bdaa67ce04a
takitajwar17 <tajwaruzzaman@iut-dhaka.edu>
2025-05-02 21:51:32 +0600

    Add Logo component and logo image for navigation

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const Logo = () => {
+   const router = useRouter();
+
+   return (
+      <Image
+         onClick={() => router.push("/")}
+         alt="Logo"
+         className="hidden md:block cursor-pointer"
+         width={120}
+         height={120}
+         src="/images/logo.png"
+      />
+   );
+};
+
+export default Logo;
