30442d102136d10d06fe63f70c85357dff5e9b9f
takitajwar17 <tajwaruzzaman@iut-dhaka.edu>
2025-05-02 21:39:13 +0600

    Initialize Next.js project with essential configurations and dependencies

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+   images: {
+      remotePatterns: [
+         { hostname: "avatars.githubusercontent.com" },
+         { hostname: "lh3.googleusercontent.com" },
+         { hostname: "res.cloudinary.com" }
+      ],
+   },
+};
+
+module.exports = nextConfig;
